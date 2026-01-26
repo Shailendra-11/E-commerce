c32d77a9ea5adb71a79107644917bf1871caa597
Shailendra Rai <shailendrarai153@gmail.com>
2026-01-26 23:35:48 +0530

    start inital

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Button } from "./components/ui/button"
+
+const App = () => {
+  return (
+    <div className="to-red-500">App
+    
+      <Button>hii</Button>
+    </div>
+  )
+}
+
+export default App
